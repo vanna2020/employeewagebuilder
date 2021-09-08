39f10d39ce3bd679c550be243b5f13783e74c961
vanna2020 <vannasingh1999@gmail.com>
2021-09-08 11:10:10 -0700

    calculating wages for a month

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,39 +2,52 @@ package com.company;
 import java.util.Random;
 public class Main {
 
-        public static final int IS_FULL_TIME = 2;
-        public static final int IS_PART_TIME = 1;
-        public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NO_OF_WORKING_DAYS = 20;
 
-        public static void main (String[]args){
-            int IS_FULL_TIME1 = 2;
-            int IS_PART_TIME2 = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int empHours = 0;
-            int empWage = 0;
-            // Taking Random values 0 or 1.
-            Random random = new Random();
-            int empCheck = random.nextInt(3);
-            // Checking the condition.
-            if (empCheck == IS_FULL_TIME) {
+    public static void main(String[] args) {
+        int empHours = 0;
+        int empWage = 0;
+        int totalEmployeeWage = 0;
+        // Taking Random values 0 or 1.
+        Random random = new Random();
+        int empCheck = random.nextInt(3);
+        switch (empCheck) {
+            case IS_FULL_TIME:
                 empHours = 8;
-            } else if (empCheck == IS_PART_TIME) {
+                break;
+            case IS_PART_TIME:
                 empHours = 4;
-            } else {
+                break;
+            default:
                 empHours = 0;
-                switch (empCheck) {
-                    case IS_FULL_TIME:
-                        empHours = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHours = 4;
-                        break;
-                    default:
-                        empHours = 0;
+
+                for (int day = 1; day <= NO_OF_WORKING_DAYS; day++) {
+                    Random = new Random();
+                    int empCheck1 = random.nextInt(3);
+                    switch (empCheck) {
+                        case IS_FULL_TIME:
+                            empHours = 8;
+                            System.out.print("The wages of Full Time employee is : ");
+                            break;
+                        case IS_PART_TIME:
+                            empHours = 4;
+                            System.out.print("The wages of Part time employee is : ");
+                            break;
+                        default:
+                            empHours = 0;
+                            System.out.print("The wages of no work employee is : ");
+                    }
+                    empWage = empHours * EMP_RATE_PER_HOUR;
+                    totalEmployeeWage += empWage;
+                    System.out.println(empWage);
                 }
                 empWage = empHours * EMP_RATE_PER_HOUR;
                 System.out.println("The daily wages of an employee is : " + empWage);
-            }
+                System.out.println();
+                System.out.print("The total wages of all the emplpoyee for 20 days is : " + totalEmployeeWage);
         }
     }
 }
