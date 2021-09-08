8bd498a1a04ffe20626e828e09629654c85ce3ce
vanna2020 <vannasingh1999@gmail.com>
2021-09-08 10:35:37 -0700

    check employee is present or absent

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,22 @@ import java.util.Random;
 public class Main {
 
             public static void main(String[] args) {
-                System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-                int IS_FULL_TIME = 1;
-                Random random = new Random();
-                int empCheck = random.nextInt(2);
-                if (empCheck == IS_FULL_TIME){
-                    System.out.println("Employee is Present");
-                }
-                else {
-                    System.out.println("Employee is Absent");
+                    int IS_FULL_TIME = 1;
+                    int EMP_RATE_PER_HOUR = 20;
+                    int empHours = 0;
+                    int empWage = 0;
+
+                    Random random = new Random();
+                    int empCheck = random.nextInt(2);
+                    if (empCheck == IS_FULL_TIME){
+                        System.out.println("Employee is Present");
+                        empHours = 8;
+                    }
+                    else {
+                        System.out.println("Employee is Absent");
+                        empHours = 0;
+                    }
+                    empWage = empHours * EMP_RATE_PER_HOUR;
+                    System.out.println("The daily wages of an employee is : " + empWage);
                 }
             }
-        }
